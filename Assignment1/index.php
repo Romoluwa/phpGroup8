38de9798cfcc9f1b4470c5bbcb549a1edfbd6557
Romoluwa <157986636+Romoluwa@users.noreply.github.com>
2025-10-24 10:05:10 +0100

    Revert "updated UI design and improved login and register page styling (#2)"
    This reverts commit 35ec80992af3cc23d856be034ff844a60cdcaedf.

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -2,42 +2,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Voting System - Login</title>
-
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-
-  <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-  <div class="container">
-    <div class="card">
-      <h1>Welcome To Babcock’s Voting Portal</h1>
-      <h3>Login to your account</h3>
-
-      <form action="login.php" method="POST">
-        <input type="text" name="firstname" placeholder="Enter your first name" required>
-        <input type="text" name="lastname" placeholder="Enter your last name" required>
-        <input type="text" name="matricno" placeholder="e.g., AB20/00123" required>
-        <input type="password" name="password" placeholder="Enter your password" required>
-
-        <select name="role" required>
-          <option value="">Select Role</option>
-          <option value="candidate">Candidate</option>
-          <option value="voter">Voter</option>
-        </select>
-
-        <button type="submit" class="btn-primary">Login</button>
-      </form>
-
-      <p class="login-text">Don’t have an account? 
-        <a href="registration.php">Register here</a>
-      </p>
-    </div>
-  </div>
-</body>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online voting system</title>
